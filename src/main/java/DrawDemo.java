1e7f1eda2cc5b707b5c4d8d2c7bfd9e24a861918
Jelle De Boeck <Jelle.DeBoeck@gmail.com>
2020-10-02 12:01:08 +0200

    First Commit + DrawDemo Completed

---
+++ src/main/java/DrawDemo.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class DrawDemo extends Frame {
+
+    public DrawDemo(){
+        super("Computer Graphics Demo");
+        prepareGUI();
+    }
+
+    public static void main(String[] args){
+        DrawDemo drawDemo = new DrawDemo();
+        drawDemo.setVisible(true);
+    }
+
+    private void prepareGUI(){
+        setSize(400,400);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        int x = 50;
+        int y = 50;
+        drawPoint(g2, x, y);
+        x = 70;
+        y = 80;
+        drawPoint(g2, x, y);
+    }
+
+    public void drawPoint(Graphics2D g2, int x, int y){
+        g2.drawLine(x, y, x, y);
+    }
+}
