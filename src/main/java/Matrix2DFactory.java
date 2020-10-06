0df47b93e32a6dfb9913f17bff568b112e4d00e2
Jelle <jelle.deboeck@gmail.com>
2020-10-06 17:56:53 +0200

    Added first matrix creation function in 2D factory

--- src/main/java/Matrix2DFactory.java
+++ src/main/java/Matrix2DFactory.java
@@ -1,5 +1,13 @@
 public class Matrix2DFactory {
     public Matrix2DFactory() {
     }
-    
+    public Matrix create_trans_matrix(double m13, double m23){
+        double[][] trans_matrix = {
+                new double[]{0, 0, m13},
+                new double[]{0, 0, m23},
+                new double[]{0, 0, 1}
+
+        };
+        return new Matrix(trans_matrix);
+    }
 }
