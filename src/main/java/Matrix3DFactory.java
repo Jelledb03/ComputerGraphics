e6c44e4194fd80452bd875ad5e797569eb412a59
Jelle <jelle.deboeck@gmail.com>
2020-10-07 15:31:23 +0200

    Finished adding functions to Matrix 3D factory

--- src/main/java/Matrix3DFactory.java
+++ src/main/java/Matrix3DFactory.java
@@ -35,10 +35,10 @@ public class Matrix3DFactory {
         return new Matrix(transf_matrix);
     }
     public Matrix create_inv_x_roll_matrix(double beta){
-        double m22 = (cos(2*beta) + 1) /(2*cos(beta));
+        //double m22 = (cos(2*beta) + 1) /(2*cos(beta));
         double[][] transf_matrix = {
                 new double[]{1, 0, 0, 0},
-                new double[]{0, m22, sin(beta), 0},
+                new double[]{0, cos(beta), sin(beta), 0},
                 new double[]{0, -sin(beta), cos(beta), 0},
                 new double[]{0, 0, 0, 1}
         };
@@ -55,12 +55,32 @@ public class Matrix3DFactory {
         return new Matrix(transf_matrix);
     }
     public Matrix create_inv_y_roll_matrix(double beta){
-        double m22 = (cos(2*beta) + 1) /(2*cos(beta));
+        //double m22 = (cos(2*beta) + 1) /(2*cos(beta));
         double[][] transf_matrix = {
-                new double[]{m22, 0, -sin(beta), 0},
+                new double[]{cos(beta), 0, -sin(beta), 0},
                 new double[]{0, 1, 0, 0},
                 new double[]{sin(beta), 0, cos(beta), 0},
                 new double[]{0, 0, 0, 1}
+        };
+        return new Matrix(transf_matrix);
+    }
+    public Matrix create_z_roll_matrix(double beta){
+        double[][] transf_matrix = {
+                new double[]{cos(beta), -sin(beta), 0, 0},
+                new double[]{sin(beta), cos(beta), 0, 0},
+                new double[]{0, 0, 1, 0},
+                new double[]{0, 0, 0, 1}
+
+        };
+        return new Matrix(transf_matrix);
+    }
+    public Matrix create_inv_z_roll_matrix(double beta){
+        double[][] transf_matrix = {
+                new double[]{cos(beta), sin(beta), 0, 0},
+                new double[]{-sin(beta), cos(beta), 0, 0},
+                new double[]{0, 0, 1, 0},
+                new double[]{0, 0, 0, 1}
+
         };
         return new Matrix(transf_matrix);
     }
