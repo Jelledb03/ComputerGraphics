e0008853f38af02929ce981e44e00785ad7a3708
Jelle <jelle.deboeck@gmail.com>
2020-10-29 19:35:53 +0100

    Translation matrix verandert naar scaling

--- src/main/java/testWorld.java
+++ src/main/java/testWorld.java
@@ -11,7 +11,7 @@ public class testWorld {
         Matrix3DFactory matrix3DFactory = new Matrix3DFactory();
         Camera camera = new Camera();
         //afstand van camera tot de viewpoint
-        camera.set_N(100);
+        camera.set_N(1000);
         Point eye = new Point(10, 10, 10);
         camera.set_eye(eye);
         Vector n = new Vector(1, 1, 1);
